caa9ac76ff2ff69a04ec07d48f58756bd8497a37
Eric Liu <erichyliu08@gmail.com>
2025-06-20 18:48:27 -0400

    added fake test

--- test.py
+++ test.py
@@ -5,5 +5,8 @@ class TestMethod(unittest.TestCase):
     def test_value(self):
         self.assertEqual(hello_world(), "hello world")
 
+    def test_fake(self):
+        self.assertNotEqual(hello_world(), "hello world")
+
 if __name__  == '__main__':
     unittest.main()
